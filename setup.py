198403cea74fcf58a89b3a1418d8e1d7aa2c1762
encorehu <huyoo353@126.com>
2014-10-28 19:14:34 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name = "Repo",
+    version = '0.1.0',
+    license = 'MIT',
+    url = 'http://github.com/encorehu/repo/',
+    author = 'Encore Hu',
+    author_email = 'huyoo353@126.com',
+    description = 'description',
+    long_description = 'long description',
+    test_suite = 'tests',
+    download_url = '',
+    packages = packages,
+    cmdclass = cmdclasses,
+    data_files = data_files,
+)
